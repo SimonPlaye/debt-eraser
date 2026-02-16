93db8cd2bab4dc2c6fe199cf936773231476d67b
SimonPlaye <playesimon@gmail.com>
2026-02-16 22:18:57 +0100

    chore: update capital with previous capital

--- src/pages/CapitalTaxPage/getGraphData.ts
+++ src/pages/CapitalTaxPage/getGraphData.ts
@@ -13,12 +13,21 @@ import {
   WEIGHT_LOWEST_BOUNDARY,
   WEIGHT_UPPER_BOUNDARY,
 } from "../../shared/constants";
+import { formatToGraphData } from "../../shared/formatToGraphData";
 import type { TaxLevelPerWealthGroup, TaxLevels } from "./types";
 
 export const getGraphData = ({ taxLevels }: TaxLevels) => {
   let deficit = DEFICIT_LEVEL_IN_2024;
   let debt = DEBT_LEVEL_IN_2024;
   let gdp = GDP_LEVEL_IN_2024;
+  let {
+    capital1mTo5m,
+    capital5mTo10m,
+    capital10mTo50m,
+    capital50mTo100m,
+    capital100mTo500m,
+    capitalFrom500m,
+  } = getInitialValues();
 
   const debtToGdpRatios: number[] = [debt / gdp];
   const deficitToGdpRatios: number[] = [deficit / gdp];
@@ -29,12 +38,23 @@ export const getGraphData = ({ taxLevels }: TaxLevels) => {
       deficit,
       debt,
       gdp,
-      yearIndex,
+      capital1mTo5m,
+      capital5mTo10m,
+      capital10mTo50m,
+      capital50mTo100m,
+      capital100mTo500m,
+      capitalFrom500m,
     });
 
     deficit = yearValues.deficit;
     debt = yearValues.debt;
     gdp = yearValues.gdp;
+    capital1mTo5m = yearValues.newCapital1mTo5m;
+    capital5mTo10m = yearValues.newCapital5mTo10m;
+    capital10mTo50m = yearValues.newCapital10mTo50m;
+    capital50mTo100m = yearValues.newCapital50mTo100m;
+    capital100mTo500m = yearValues.newCapital100mTo500m;
+    capitalFrom500m = yearValues.newCapitalFrom500m;
 
     debtToGdpRatios.push(debt / gdp);
     deficitToGdpRatios.push(deficit / gdp);
@@ -43,116 +63,202 @@ export const getGraphData = ({ taxLevels }: TaxLevels) => {
   return formatToGraphData({ debtToGdpRatios, deficitToGdpRatios });
 };
 
+const getInitialValues = () => {
+  const averageWealthForGroup1mTo5m =
+    1 * WEIGHT_LOWEST_BOUNDARY + 5 * WEIGHT_UPPER_BOUNDARY;
+  const averageWealthForGroup5mTo10m =
+    5 * WEIGHT_LOWEST_BOUNDARY + 10 * WEIGHT_UPPER_BOUNDARY;
+  const averageWealthForGroup10mTo50m =
+    10 * WEIGHT_LOWEST_BOUNDARY + 50 * WEIGHT_UPPER_BOUNDARY;
+  const averageWealthForGroup50mTo100m =
+    50 * WEIGHT_LOWEST_BOUNDARY + 100 * WEIGHT_UPPER_BOUNDARY;
+  const averageWealthForGroup100mTo500m =
+    100 * WEIGHT_LOWEST_BOUNDARY + 500 * WEIGHT_UPPER_BOUNDARY;
+  const averageWealthForGroupFrom500m = 500;
+  return {
+    capital1mTo5m:
+      (averageWealthForGroup1mTo5m * HOUSEHOLD_NB_BETWEEN_1M_AND_5M) / 1000,
+    capital5mTo10m:
+      (averageWealthForGroup5mTo10m * HOUSEHOLD_NB_BETWEEN_5M_AND_10M) / 1000,
+    capital10mTo50m:
+      (averageWealthForGroup10mTo50m * HOUSEHOLD_NB_BETWEEN_10M_AND_50M) / 1000,
+    capital50mTo100m:
+      (averageWealthForGroup50mTo100m * HOUSEHOLD_NB_BETWEEN_50M_AND_100M) /
+      1000,
+    capital100mTo500m:
+      (averageWealthForGroup100mTo500m * HOUSEHOLD_NB_BETWEEN_100M_AND_500M) /
+      1000,
+    capitalFrom500m:
+      (averageWealthForGroupFrom500m * HOUSEHOLD_NB_WITH_MORE_THAN_500M) / 1e3,
+  };
+};
+
 const getAggregateValues = ({
   taxLevels,
   deficit,
   debt,
   gdp,
-  yearIndex,
+  capital1mTo5m,
+  capital5mTo10m,
+  capital10mTo50m,
+  capital50mTo100m,
+  capital100mTo500m,
+  capitalFrom500m,
 }: {
   taxLevels: TaxLevelPerWealthGroup;
   deficit: number;
   debt: number;
   gdp: number;
-  yearIndex: number;
+  capital1mTo5m: number;
+  capital5mTo10m: number;
+  capital10mTo50m: number;
+  capital50mTo100m: number;
+  capital100mTo500m: number;
+  capitalFrom500m: number;
 }) => {
-  const taxedRevenueInBillions = getTaxedRevenueInBillions({
+  const {
+    newCapital1mTo5m,
+    newCapital5mTo10m,
+    newCapital10mTo50m,
+    newCapital50mTo100m,
+    newCapital100mTo500m,
+    newCapitalFrom500m,
+    totalRevenueInBillions,
+  } = getTaxedRevenueInBillions({
     taxLevels,
-    yearIndex,
+    capital1mTo5m,
+    capital5mTo10m,
+    capital10mTo50m,
+    capital50mTo100m,
+    capital100mTo500m,
+    capitalFrom500m,
   });
 
   const newDeficitLevel =
-    deficit * (1 + AVERAGE_GDP_EVOLUTION_PER_YEAR) - taxedRevenueInBillions;
+    deficit * (1 + AVERAGE_GDP_EVOLUTION_PER_YEAR) - totalRevenueInBillions;
 
   return {
     debt: Math.max(debt + newDeficitLevel, 0),
     deficit: newDeficitLevel,
     gdp: gdp * (1 + AVERAGE_GDP_EVOLUTION_PER_YEAR),
+    newCapital1mTo5m,
+    newCapital5mTo10m,
+    newCapital10mTo50m,
+    newCapital50mTo100m,
+    newCapital100mTo500m,
+    newCapitalFrom500m,
   };
 };
 
 const getTaxedRevenueInBillions = ({
   taxLevels,
-  yearIndex,
+  capital1mTo5m,
+  capital5mTo10m,
+  capital10mTo50m,
+  capital50mTo100m,
+  capital100mTo500m,
+  capitalFrom500m,
 }: {
   taxLevels: TaxLevelPerWealthGroup;
-  yearIndex: number;
-}): number => {
-  const averageWealthForGroup1mTo5m =
-    1 * WEIGHT_LOWEST_BOUNDARY + 5 * WEIGHT_UPPER_BOUNDARY;
-  const revenueFromGroup1mTo5m =
-    taxLevels.taxLevelGroup1mTo5m *
-    (averageWealthForGroup1mTo5m *
-      HOUSEHOLD_NB_BETWEEN_1M_AND_5M *
-      (1 + CAPITAL_GROWTH_RATE - taxLevels.taxLevelGroup1mTo5m) ** yearIndex);
+  capital1mTo5m: number;
+  capital5mTo10m: number;
+  capital10mTo50m: number;
+  capital50mTo100m: number;
+  capital100mTo500m: number;
+  capitalFrom500m: number;
+}) => {
+  const newCapital1mTo5m = getNewCapitalAmount({
+    oldValue: capital1mTo5m,
+    taxLevel: taxLevels.taxLevelGroup1mTo5m,
+  });
+  const revenueFromGroup1mTo5m = getTaxedAmount({
+    oldValue: capital1mTo5m,
+    taxLevel: taxLevels.taxLevelGroup1mTo5m,
+  });
 
-  const averageWealthForGroup5mTo10m =
-    5 * WEIGHT_LOWEST_BOUNDARY + 10 * WEIGHT_UPPER_BOUNDARY;
-  const revenueFromGroup5mTo10m =
-    taxLevels.taxLevelGroup5mTo10m *
-    averageWealthForGroup5mTo10m *
-    HOUSEHOLD_NB_BETWEEN_5M_AND_10M *
-    (1 + CAPITAL_GROWTH_RATE - taxLevels.taxLevelGroup5mTo10m) ** yearIndex;
+  const newCapital5mTo10m = getNewCapitalAmount({
+    oldValue: capital5mTo10m,
+    taxLevel: taxLevels.taxLevelGroup5mTo10m,
+  });
+  const revenueFromGroup5mTo10m = getTaxedAmount({
+    oldValue: capital5mTo10m,
+    taxLevel: taxLevels.taxLevelGroup5mTo10m,
+  });
 
-  const averageWealthForGroup10mTo50m =
-    10 * WEIGHT_LOWEST_BOUNDARY + 50 * WEIGHT_UPPER_BOUNDARY;
-  const revenueFromGroup10mTo50m =
-    taxLevels.taxLevelGroup10mTo50m *
-    averageWealthForGroup10mTo50m *
-    HOUSEHOLD_NB_BETWEEN_10M_AND_50M *
-    (1 + CAPITAL_GROWTH_RATE - taxLevels.taxLevelGroup10mTo50m) ** yearIndex;
+  const newCapital10mTo50m = getNewCapitalAmount({
+    oldValue: capital10mTo50m,
+    taxLevel: taxLevels.taxLevelGroup10mTo50m,
+  });
+  const revenueFromGroup10mTo50m = getTaxedAmount({
+    oldValue: capital10mTo50m,
+    taxLevel: taxLevels.taxLevelGroup10mTo50m,
+  });
 
-  const averageWealthForGroup50mTo100m =
-    50 * WEIGHT_LOWEST_BOUNDARY + 100 * WEIGHT_UPPER_BOUNDARY;
-  const revenueFromGroup50mTo100m =
-    taxLevels.taxLevelGroup50mTo100m *
-    averageWealthForGroup50mTo100m *
-    HOUSEHOLD_NB_BETWEEN_50M_AND_100M *
-    (1 + CAPITAL_GROWTH_RATE - taxLevels.taxLevelGroup50mTo100m) ** yearIndex;
+  const newCapital50mTo100m = getNewCapitalAmount({
+    oldValue: capital50mTo100m,
+    taxLevel: taxLevels.taxLevelGroup50mTo100m,
+  });
+  const revenueFromGroup50mTo100m = getTaxedAmount({
+    oldValue: capital50mTo100m,
+    taxLevel: taxLevels.taxLevelGroup50mTo100m,
+  });
 
-  const averageWealthForGroup100mTo500m =
-    100 * WEIGHT_LOWEST_BOUNDARY + 500 * WEIGHT_UPPER_BOUNDARY;
-  const revenueFromGroup100mTo500m =
-    taxLevels.taxLevelGroup100mTo500m *
-    averageWealthForGroup100mTo500m *
-    HOUSEHOLD_NB_BETWEEN_100M_AND_500M *
-    (1 + CAPITAL_GROWTH_RATE - taxLevels.taxLevelGroup100mTo500m) ** yearIndex;
+  const newCapital100mTo500m = getNewCapitalAmount({
+    oldValue: capital100mTo500m,
+    taxLevel: taxLevels.taxLevelGroup100mTo500m,
+  });
+  const revenueFromGroup100mTo500m = getTaxedAmount({
+    oldValue: capital100mTo500m,
+    taxLevel: taxLevels.taxLevelGroup100mTo500m,
+  });
 
-  const averageWealthForGroupFrom500m = 500;
-  const revenueFromGroupFrom500m =
-    taxLevels.taxLevelGroupFrom500m *
-    averageWealthForGroupFrom500m *
-    HOUSEHOLD_NB_WITH_MORE_THAN_500M *
-    (1 + CAPITAL_GROWTH_RATE - taxLevels.taxLevelGroupFrom500m) ** yearIndex;
+  const newCapitalFrom500m = getNewCapitalAmount({
+    oldValue: capitalFrom500m,
+    taxLevel: taxLevels.taxLevelGroupFrom500m,
+  });
+  const revenueFromGroupFrom500m = getTaxedAmount({
+    oldValue: capitalFrom500m,
+    taxLevel: taxLevels.taxLevelGroupFrom500m,
+  });
 
-  return (
-    (revenueFromGroup1mTo5m +
-      revenueFromGroup5mTo10m +
-      revenueFromGroup10mTo50m +
-      revenueFromGroup50mTo100m +
-      revenueFromGroup100mTo500m +
-      revenueFromGroupFrom500m) /
-    1000
-  );
+  const totalRevenueInBillions =
+    revenueFromGroup1mTo5m +
+    revenueFromGroup5mTo10m +
+    revenueFromGroup10mTo50m +
+    revenueFromGroup50mTo100m +
+    revenueFromGroup100mTo500m +
+    revenueFromGroupFrom500m;
+
+  return {
+    newCapital1mTo5m,
+    newCapital5mTo10m,
+    newCapital10mTo50m,
+    newCapital50mTo100m,
+    newCapital100mTo500m,
+    newCapitalFrom500m,
+    totalRevenueInBillions,
+  };
 };
 
-const formatToGraphData = ({
-  debtToGdpRatios,
-  deficitToGdpRatios,
+export const getNewCapitalAmount = ({
+  oldValue,
+  taxLevel,
 }: {
-  debtToGdpRatios: number[];
-  deficitToGdpRatios: number[];
+  oldValue: number;
+  taxLevel: number;
 }) => {
-  return [
-    {
-      name: "Debt to GDP Ratio",
-      data: debtToGdpRatios,
-      color: "#1a4d8f",
-    },
-    {
-      name: "Deficit to GDP Ratio",
-      data: deficitToGdpRatios,
-      color: "#ed2939",
-    },
-  ];
+  const newCapitalBeforeTax = oldValue * (1 + CAPITAL_GROWTH_RATE);
+  const taxAmount = newCapitalBeforeTax * taxLevel;
+  return newCapitalBeforeTax - taxAmount;
+};
+
+export const getTaxedAmount = ({
+  oldValue,
+  taxLevel,
+}: {
+  oldValue: number;
+  taxLevel: number;
+}) => {
+  const newCapitalBeforeTax = oldValue * (1 + CAPITAL_GROWTH_RATE);
+  return newCapitalBeforeTax * taxLevel;
 };
